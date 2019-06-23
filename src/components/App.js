fffb517a4a225115b510ad5199a741e1f49c42b7
Lanzhen Wang <lanzhenwang@Lanzhens-MacBook-Pro.local>
2019-06-23 00:08:10 -0500

    added navbar

--- src/components/App.js
+++ src/components/App.js
@@ -6,21 +6,28 @@ import SearchLinks from './Link/SearchLinks';
 import Login from './Auth/Login';
 import ForgotPassword from './Auth/ForgotPassword';
 import LinkDetail from './Link/LinkDetail';
+import Header from './Header';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path='/' render={()=> <Redirect to='/new/1'/>} component={CreateLink} />
-        <Route path='/create' component={CreateLink} />
-        <Route path='/login' component={Login} />
-        <Route path='/forgot' component={ForgotPassword} />
-        <Route path='/top' component={LinkList} />
-        <Route path='/search' component={SearchLinks} />
-        <Route path='/new/:page' component={LinkList} />
-        <Route path='/link/:linkId' component={LinkDetail} />
-      </Switch>
+      <div className="app-container">
+        <Header />
+        <div className="route-container">
+          <Switch>
+            <Route exact path='/' render={()=> <Redirect to='/new/1'/>} component={CreateLink} />
+            <Route path='/create' component={CreateLink} />
+            <Route path='/login' component={Login} />
+            <Route path='/forgot' component={ForgotPassword} />
+            <Route path='/top' component={LinkList} />
+            <Route path='/search' component={SearchLinks} />
+            <Route path='/new/:page' component={LinkList} />
+            <Route path='/link/:linkId' component={LinkDetail} />
+          </Switch>
+        </div>
+      </div>
+      
     </BrowserRouter>
   );
 }
