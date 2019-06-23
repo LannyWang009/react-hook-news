02660be4c40bd2e4b0605e8440489fecce912d97
Lanzhen Wang <lanzhenwang@Lanzhens-MacBook-Pro.local>
2019-06-23 01:56:38 -0500

    added onSubmit and onChange to the form

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,6 +1,16 @@
 import React from "react";
+import useFormValidation from "./useFormValidation";
+
+const INITIAL_STATE = {
+  // this is an object so that we can add different state later
+  name: '',
+  email: '',
+  password: ''
+}
 
 function Login(props) {
+  const {handleChange, handleSubmit, values} = useFormValidation(INITIAL_STATE)
+
   const [login, setLogin] = React.useState(true)
 
   return (
@@ -8,10 +18,28 @@ function Login(props) {
       <h2 className='mv3'>
        {login ? "Login" : "Create an account"}
       </h2>
-      <form className='flex flex-column'>
-        {!login && <input type='text' placeholder='Your Name' autoComplete="off"/>}
-        <input type="email" placeholder="Your Email" autoComplete='off'/>
-        <input type='password' placeholder="Choose a secure password" />
+      <form onSubmit={handleSubmit}
+      className='flex flex-column'>
+        {!login && <input 
+        type='text' 
+        onChange={handleChange}
+        name="name"
+        value = {values.name}
+        placeholder='Your Name' 
+        autoComplete="off"/>}
+        <input 
+        type="email" 
+        onChange={handleChange}
+        value={values.email}
+        name="email"
+        placeholder="Your Email" 
+        autoComplete='off'/>
+        <input 
+        type='password' 
+        value={values.password}
+        onChange={handleChange}
+        name="password"
+        placeholder="Choose a secure password" />
         <div className='flex mt3'>
           <button type='submit' className='button pointer mr2'>
             Submit!
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,23 @@
 import React from "react";
 
-function useFormValidation() {}
+function useFormValidation(initialState) {
+    // to do form validation
+    const [values, setValues] = React.useState(initialState)
+    var handleChange = event => {
+        event.persist() // ? what does it do?
+        setValues(previousValues => ({
+            ...previousValues, 
+            [event.target.name]:event.target.value
+            })
+        )}
+    
+
+    var handleSubmit = event => {
+        event.preventDefault();
+        console.log({values})
+    }
+
+    return {handleChange, handleSubmit, values}
+}
 
 export default useFormValidation;
